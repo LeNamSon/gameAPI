6aacb4bbefbbab2548179d808fd9beeff3ebc81f
Le Nam Son <lenamson@gmail.com>
2021-08-13 17:36:30 +0700

    Modified gender coin for users table by migration

---
+++ database/migrations/2021_08_13_102630_add_more_cols_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMoreColsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedTinyInteger('gender')->default(0)->after('remember_token');
+            $table->unsignedInteger('coin')->default(0)->after('gender'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('gender');
+            $table->dropColumn('coin');
+        });
+    }
+}
